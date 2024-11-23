a23914aba47ca73be64a4c27b6171185391c11ce
meng <mfrwyy@163.com>
2024-11-23 10:34:46 +0800

    feat: 配置 husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     rules: {
       // 单引号
-      quotes: ["error", "single"],
+      quotes: ['error', 'single'],
       // 最多允许 2 行空行
-      "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 0 }],
+      'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
       // 声明未使用的变量只允许 React、App
-      "no-unused-vars": ["warn", { varsIgnorePattern: "^(React|App)$" }],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^(React|App)$' }],
     },
     settings: {
       react: {
         // pluginReact 与 eslintConfigPrettier 同时使用会有报错
         // 1、错误：Warning: React version not specified in eslint-plugin-react settings.
         // 2、处理：指定 React 版本（自动检测 React 版本）
-        version: "detect",
+        version: 'detect',
       },
     },
   },
