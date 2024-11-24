98c236b0a7b332aecec43a3648acbf3b511a8013
meng <mfrwyy@163.com>
2024-11-24 16:51:31 +0800

    feat: 配置 typescript 并结合 eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,21 @@ import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
   {
     rules: {
       // 单引号
